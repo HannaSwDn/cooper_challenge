a254e8c12bb162c1d006f552f15eddd56e45f7ee
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-11 15:33:55 +0100

    Adds tests for PerformanceData

--- db/migrate/20181211134912_create_performance_data.rb
+++ db/migrate/20181211134912_create_performance_data.rb
@@ -1,5 +1,6 @@
 class CreatePerformanceData < ActiveRecord::Migration[5.2]
   def change
+    execute 'CREATE EXTENSION IF NOT EXISTS hstore'
     create_table :performance_data do |t|
       t.references :user, foreign_key: true
       t.hstore :data
