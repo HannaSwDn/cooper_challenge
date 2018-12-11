8360956475d6dbc4bc29ba394827214674843a7e
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-11 13:33:47 +0100

    Adds tests for user registrations

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { 'techinterviews@email.com' }
+    email { 'hanna@email.com' }
     password { 'password' }
     password_confirmation { 'password' }
   end
